96f11a12b6a464f178743187319274209c1b6bdf
MBoussel <mb.boussel@gmail.com>
2025-07-23 17:51:49 +0200

    feat: middelware create for skills

--- router.ts
+++ router.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import projectActions from "./project/projectActions";
 import skillActions from "./skill/skillActions";
-import validateProject from "./middlewares/validateProject";
+import validateProjects from "./middlewares/validateProject";
+import validateSkills from "./middlewares/validateSkill";
 
 
 const router =express.Router();
 //Routes Projects
 router.get("/api/projects", projectActions.browse);
 router.get("/api/projects/:id", projectActions.read);
-router.post("/api/projects",validateProject, projectActions.add);
-router.put("/api/projects/:id",validateProject, projectActions.edit);
+router.post("/api/projects",validateProjects, projectActions.add);
+router.put("/api/projects/:id",validateProjects, projectActions.edit);
 router.delete("/api/projects/:id", projectActions.destroy);
 
 //Routes Skills
 router.get("/api/skills", skillActions.browse)
 router.get("/api/skills/:id", skillActions.read);
-router.post("/api/skills",skillActions.add)
+router.post("/api/skills",validateSkills, skillActions.add)
 
 export default router
